686d34393f72783c40c8a72b13ebcdccbf272bad
reha96 <51120957+reha96@users.noreply.github.com>
2026-04-16 14:54:52 +0200

    Update web.sh

--- Course 3 - BY/app_v2/web.sh
+++ Course 3 - BY/app_v2/web.sh
@@ -3,4 +3,4 @@
 chmod +x web.sh 2>/dev/null || true
 
 # Start gunicorn with production settings
-gunicorn main:app --bind 0.0.0.0:$PORT --workers 2 --threads 4
+# gunicorn main:app --bind 0.0.0.0:$PORT --workers 2 --threads 4
